2a3297d2f57ca341a31b35613890b975338c2c73
Lijun Liu <lijun.liu@thoughtworks.com>
2019-03-11 20:36:52 +0800

    upload code

---
+++ src/main/scala/com/selflearning/spark/HelloSpark.scala
@@ -0,0 +1,25 @@
+package com.selflearning.spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object HelloSpark {
+
+  def main(args: Array[String]): Unit = {
+    if (args.length < 2) {
+      println("please set input file and output file")
+      System.exit(1)
+    }
+
+    val sparkConf = new SparkConf().setMaster("local").setAppName("HelloSpark")
+    val sc = new SparkContext(sparkConf)
+    val input = sc.textFile(args(0))
+    val words = input.flatMap(line => line.split(" "))
+    val counts = words.map(word => (word, 1)).reduceByKey {
+      case (x, y) => x + y
+    }
+
+    counts.saveAsTextFile(args(1))
+
+    sc.stop()
+  }
+}
